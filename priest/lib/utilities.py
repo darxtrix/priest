ce4a53dc01681416daada8d0d0a76242ddc048a3
Ankush Sharma <ankprashar@gmail.com>
2015-06-24 01:59:09 +0530

    Added image functionality

--- priest/lib/utilities.py
+++ priest/lib/utilities.py
@@ -4,6 +4,18 @@
 
 import requests, goslate
 import os
+from PIL import Image, ImageFont, ImageDraw, time
+
+def get_coordinates():
+    '''
+        Returns the lattitude & longitude pairs
+    '''
+
+def get_time(lattitude,longitude):
+    '''
+        Find the current time at the location pointed out by the lattitude and longitude
+    '''
+
 
 def get_location():
     ''' 
@@ -34,4 +46,25 @@ def translate_message(msg,language_code):
     '''
     gs = goslate.Goslate()
     return gs.translate(msg,language_code)
+
+
+def prepare_image(msg,img_path,font):
+    '''
+        Prepare the image by writing text to it by using the supplied font
+        @param  img  The image path
+        @param  msg  The message need to be written on the image
+        @param  font  The font to be used ( "the ttf path /data/fonts/clearsans.ttf")
+    '''
+    img = Image.open(img_path)
+    draw = ImageDraw.Draw(img)
+    font = ImageFont(font,18)
+    width,height = img.size
+    # writing on the image
+    draw.text((int(height*0.75),int(width/10.0)),
+             'Sample text',(255,255,255),font=font)
+    # wtf is to be done with the image
+    path = os.getabspath('../data/user_data/{0}'.format(str( int(time.time()) )))
+    img.save(path)
+    return path
+
 
