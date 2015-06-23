46103388985e36067eb599c224d067077d0c646b
Ankush Sharma <ankprashar@gmail.com>
2015-06-23 21:23:30 +0530

    added message generator

---
+++ priest/lib/msg_generater.py
@@ -0,0 +1,37 @@
+'''
+    Generate messages by using from some wishing templates and synonyms list
+'''
+import random
+
+GLOBAL_SYNONYM_DICT = {
+    "good" : ['great','marvelous','blissful','positive','superb','nice','wonderful','splendid'],
+    "morning" : ['aurora','forenoon','sunup','sunrise'],
+    "wish" : ['wishing'],
+    "hey" : ['hi','hello','howdy','yo','heya']
+}
+
+DELIMITERS = [':)',':-)',';-)','.','!','!!','','','']
+
+
+def genMessage(template_file):
+    '''
+        Generates a message
+        @param  template_file Template file to use for generating message
+    '''
+    f = open(template_file,'r')
+    messages_list = f.readlines()
+    f.close()
+    word_list = random.choice(messages_list).rstrip('\n').split(' ')
+    message = []
+    for s in word_list:
+        s = s.rstrip(',').rstrip('!').rstrip('!!')
+        if s in GLOBAL_SYNONYM_DICT:
+            middle_delimeter = ''
+            if s == 'hey':
+                middle_delimeter = random.choice(['!','!!',','])
+            message.append(random.choice(GLOBAL_SYNONYM_DICT[s]) + middle_delimeter)
+        else :
+            message.append(s)
+    message.append(random.choice(DELIMITERS))
+    return ' '.join(message)
+
