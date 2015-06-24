35a25378ff4762ec582500a5ee0e7205e4ad819f
Ankush Sharma <ankprashar@gmail.com>
2015-06-24 23:00:25 +0530

    final fixes

--- setup.py
+++ setup.py
@@ -3,11 +3,12 @@ from distutils.core import setup
 setup(
   name = 'priest',
   packages = ['priest'], 
-  version = 0.1,
+  version = '0.1',
   description = 'Generate wishes from your command line with full customization.',
   author = 'Ankush Sharma',
   author_email = 'ankprahsar@gmail.com',
   url = 'https://github.com/black-perl/priest', 
   download_url = 'https://github.com/black-perl/priest/tarball/0.1', 
   keywords = ['priest', 'automated-wishes'], 
+  classifiers = []
 )
